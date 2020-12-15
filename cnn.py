590e3d689824a52ce9ebd93dc1192d7eda085193
Mac station <guoliang.he@s-cube.com>
2020-12-15 18:06:38 +0000

    add

--- cnn.py
+++ cnn.py
@@ -6,32 +6,32 @@ class CNN(nn.Module):
 	def __init__(self):
 		super(CNN,self).__init__()
 		self.conv1 = nn.Conv2d(in_channels=3,
-													 out_channels = 16, 
-													 kernel_size = 3,
-													 stride = 1,
-													 padding= 1)
+								out_channels = 16, 
+								kernel_size = 3,
+								stride = 1,
+								padding= 1)
 
 		self.conv2 = nn.Conv2d(in_channels=16,
-												out_channels = 32,
-												kernel_size = 3,
-												stride = 1,
-												padding= 1)
+								out_channels = 32,
+								kernel_size = 3,
+								stride = 1,
+								padding= 1)
 		
 		self.pooling1 = nn.MaxPool2d(kernel_size=2)
 
 		self.conv3 = nn.Conv2d(in_channels=32,
-												out_channels = 16,
-												kernel_size = 3,
-												stride = 1,
-												padding= 1)
+								out_channels = 16,
+								kernel_size = 3,
+								stride = 1,
+								padding= 1)
 		
 		self.upsample = nn.Upsample(scale_factor=2)
 		
 		self.conv4 = nn.Conv2d(in_channels=16,
-												out_channels = 2,
-												kernel_size = 5,
-												stride = 1,
-												padding= 2)
+								out_channels = 2,
+								kernel_size = 5,
+								stride = 1,
+								padding= 2)
 	def forward(self,x):
 		x = x.view(1,3,256,256)
 		x = F.relu(self.conv1(x)) 

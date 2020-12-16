9868b34d34e2221848117fa5b7cc50bac7ac7492
Mac station <guoliang.he@s-cube.com>
2020-12-16 15:25:18 +0000

    fix

--- helper.py
+++ helper.py
@@ -51,16 +51,12 @@ class NN:
             local_batch, local_labels = local_batch.to(self.device), \
                                         local_labels.flatten().to(self.device)
 
-            # print('input are:')
-            # print(local_batch.size())
-            # print(local_labels.size())
+            # print('input are:'); print(local_batch.size()); print(local_labels.size())
 
             self.opt.zero_grad()
             local_output = self.model(local_batch)
 
-            # print('output are:')
-            # print(local_output.size())
-            # print(local_labels.size())
+            # print('output are:'); print(local_output.size()); print(local_labels.size())
 
             loss = self.loss(local_output, local_labels)
             loss.backward()
@@ -96,21 +92,11 @@ class NN:
         return epoch_loss
 
 
-    def prediction(self,
-                    x,
-                    ):
-
+    def prediction(self, x):
         self.model.eval()
-
         return self.model(x)
 
     def batcher(self, x, y, batch_size):
         l = len(y)
         for batch in range(0, l, batch_size):
             yield (x[batch:min(batch + batch_size, l)], y[batch:min(batch + batch_size, l)])
-    
-    def test(self, data):
-        return 
-
-
-        

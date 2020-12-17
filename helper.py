6b19c6280eca42e0e5111c34cb572a65f91abbb7
Mac station <guoliang.he@s-cube.com>
2020-12-17 12:12:02 +0000

    lazy_commie

--- helper.py
+++ helper.py
@@ -134,9 +134,10 @@ class helper:
 
         for i, d in enumerate(data):
 
-            img, label = tr.from_numpy(d[0]).float(), tr.from_numpy(d[1]).float()
+            img, label = tr.from_numpy(d[0]).unsqueeze(0).float(), \
+                            tr.from_numpy(d[1]).unsqueeze(0).float()
 
-            imgs[i][0], labels[i][0] = img, label
+            imgs[i], labels[i] = img, label
 
         X_train, X_test, y_train, y_test = train_test_split(imgs, labels, 
                                             test_size=0.33, random_state=None)
